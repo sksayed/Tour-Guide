2f3d86e5403904aecc7aa3ffa0962140738996ee
Sheikh Sayed Bin <sksayed@live.com>
2019-03-18 01:24:48 +0600

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // DECLARATION
 const express  = require('express');
+
+var app = express();
+
+const portNumber = "5000";
+
+
+
+
+//server 
+app.listen(portNumber , function( ){
+ console.log(" server started at :"+portNumber);
+});
