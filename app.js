c9d1716f33c4f6a5e532b56c22acde3ce13ba152
Sheikh Sayed Bin <sksayed@live.com>
2019-03-18 01:29:48 +0600

    body parser and express session import kora hoise

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // DECLARATION
 const express  = require('express');
+var bodyParser 		= require('body-parser');
+var exSession 		= require('express-session');
 
 var app = express();
 
 const portNumber = "5000";
 
 
+
+//setting up view engine
+app.set('view engine', 'ejs');
 
 
 //server 
